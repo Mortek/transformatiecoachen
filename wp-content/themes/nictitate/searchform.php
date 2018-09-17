20d76dee477fc072ad1799f7e4790b848cbb683b
Maurice Moret <maurice@burst-digital.com>
2018-09-17 20:53:50 +0200

    Fixed placeholder for search form

--- wp-content/themes/nictitate/searchform.php
+++ wp-content/themes/nictitate/searchform.php
@@ -1,10 +1,10 @@
 <?php
-$current_value = is_search() ? esc_attr(get_search_query()) : __('Zoeken...', kopa_get_domain());
+$current_value = is_search() ? esc_attr(get_search_query()) : __('', kopa_get_domain());
 ?>
 
 <div class="search-box clearfix">
     <form method="get" id="search-form" class="search-form clearfix" action="<?php echo esc_url(home_url('/')); ?>">                            
-        <input type="text"  value="<?php echo $current_value; ?>" name="s" class="search-text" maxlength="20">
+        <input type="text"  placeholder="Zoeken..." value="<?php echo $current_value; ?>" name="s" class="search-text" maxlength="20">
         <input type="submit" value="" name="" class="search-submit">
     </form><!-- search-form -->   
 </div><!--end:search-box-->
